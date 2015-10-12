b23ce9162c6c7ab2c8e72aefa9b23c36f7a7fc70
Lala De <lvde21@live.com>
2015-10-12 12:34:53 -0400

    added basic job, map , reduce

---
+++ Lala's/Initial Map Reduce of Strings/Reduce.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.*;
+
+public class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {	
+
+    public void reduce(Text keyname, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+        
+
+        int sum = 0;
+        for (IntWritable val : values) {
+            sum += val.get();
+        }
+
+        try {
+        	context.write(key, new IntWritable(sum));
+        }
+        catch (Exception E) {
+        	// Send signal
+        }
+    }
+}
