11cbed71d01880f0a0edc93904d7376309e71df7
stephenlienharrell <sharrell@purdue.edu>
2015-10-14 02:13:23 -0400

    anon creds

--- WeatherPipeMapReduce/src/main/java/edu/purdue/cs307/team16/S3_test1.java
+++ WeatherPipeMapReduce/src/main/java/edu/purdue/cs307/team16/S3_test1.java
@@ -6,8 +6,7 @@ import java.util.zip.GZIPInputStream;
 
 import com.amazonaws.AmazonClientException;
 import com.amazonaws.AmazonServiceException;
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.auth.AnonymousAWSCredentials;
 import com.amazonaws.regions.Region;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
@@ -25,20 +24,9 @@ public class S3_test1 {
 	
 	public static void main(String[] args) {
 
-		AWSCredentials credentials = null;
-		try {
-			credentials = new ProfileCredentialsProvider().getCredentials();
-		} 
-		catch (Exception e) {
-			throw new AmazonClientException(
-					"Cannot load the credentials from the credential profiles file. " +
-							"Please make sure that your credentials file is at the correct " +
-							"location (/Users/lalavaishnode/.aws/credentials), and is in valid format.",
-							e);
-			
-		}
 
-		AmazonS3 s3 = new AmazonS3Client(credentials);
+
+		AmazonS3 s3 = new AmazonS3Client(new AnonymousAWSCredentials());
 		Region usEast1 = Region.getRegion(Regions.US_EAST_1);
 		s3.setRegion(usEast1);
 		S3Object object;
