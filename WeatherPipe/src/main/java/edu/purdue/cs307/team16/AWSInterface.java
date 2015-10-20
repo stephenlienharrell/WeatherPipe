120871afb225b67becf8a1f09ec68d2dff303d09
stephenlienharrell <sharrell@purdue.edu>
2015-10-20 18:28:57 -0400

    Add error handling pt 2

--- WeatherPipe/src/main/java/edu/purdue/cs307/team16/AWSInterface.java
+++ WeatherPipe/src/main/java/edu/purdue/cs307/team16/AWSInterface.java
@@ -164,7 +164,28 @@ public class AWSInterface {
 	public String UploadMPJarFile(String fileLocation) {
 		String key = jobID + "WeatherPipeMapreduce.jar";
 		File jarFile = new File(fileLocation);
-		s3client.putObject(jobBucketName, key, jarFile);
+		
+		try {
+			s3client.putObject(jobBucketName, key, jarFile);
+        } catch (AmazonServiceException ase) {
+            System.out.println("Caught an AmazonServiceException, which " +
+            		"means your request made it " +
+                    "to Amazon S3, but was rejected with an error response" +
+                    " for some reason.");
+            System.out.println("Error Message:    " + ase.getMessage());
+            System.out.println("HTTP Status Code: " + ase.getStatusCode());
+            System.out.println("AWS Error Code:   " + ase.getErrorCode());
+            System.out.println("Error Type:       " + ase.getErrorType());
+            System.out.println("Request ID:       " + ase.getRequestId());
+        } catch (AmazonClientException ace) {
+            System.out.println("Caught an AmazonClientException, which " +
+            		"means the client encountered " +
+                    "an internal error while trying to " +
+                    "communicate with S3, " +
+                    "such as not being able to access the network.");
+            System.out.println("Error Message: " + ace.getMessage());
+        
+        }		
 		
 		return "s3n://" + jobBucketName + "/" + key;
 	}
