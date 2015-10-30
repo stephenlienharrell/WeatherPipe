ce71e09a57a293b01f05069803c43dfe65f2719b
Hanqi Du <Hanqi@HanqideMacBook-Pro.local>
2015-10-30 12:52:11 -0400

    user story of listing multiple buckets given range of time periods done

--- WeatherPipe/src/main/java/edu/purdue/cs307/team16/RadarFilePicker.java
+++ WeatherPipe/src/main/java/edu/purdue/cs307/team16/RadarFilePicker.java
@@ -131,6 +131,29 @@ public class RadarFilePicker {
 		
 	}
 	
+	//this function checks if the flag of the station is 4 capital letters
+	public static boolean checkStationType (String s) throws NullPointerException {
+		try {
+			if (s.isEmpty()){
+				System.out.println("error: the string is empty");
+				return false;
+			}
+			if (s.length() != 4) {
+				System.out.println("error: the length of the string is not 4");
+				return false;
+			}
+			for (int i = 0; i < s.length(); i++){
+				if (s.charAt(i) > 90 || s.charAt(i) < 65){
+					System.out.println("error: the string can only contain upper case letters");
+					return false;
+				}
+			}
+		}catch(NullPointerException e){
+			System.out.println("error: the string is null");
+			return false;
+		}
+		return true;
+	}
 	
 }
 
