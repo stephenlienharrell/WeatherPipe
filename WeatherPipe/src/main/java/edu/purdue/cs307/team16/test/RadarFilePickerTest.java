95230ef5c17761114295543efff35ef9c3cb1047
stephenlienharrell <sharrell@purdue.edu>
2015-10-30 13:07:11 -0400

    start of unittests

---
+++ WeatherPipe/src/main/java/edu/purdue/cs307/team16/test/RadarFilePickerTest.java
@@ -0,0 +1,18 @@
+package edu.purdue.cs307.team16.test;
+
+import org.junit.Test;
+
+import edu.purdue.cs307.team16.RadarFilePicker;
+import junit.framework.TestCase;
+
+public class RadarFilePickerTest extends TestCase {
+	
+	@Test
+	protected void testAddFile() {
+			// setup function
+			// run function
+			// RadarFilePicker.addFile(ret, lowBound, uppBound, station, key, awsInterface, dataBucket);
+		    // test output
+	}
+	
+}
