a3e56c0044d3d79ca03335ae3287a95e80d0b936
Hanqi Du <Hanqi@HanqideMacBook-Pro.local>
2015-11-08 14:33:29 -0500

    test file uploadInputFilelist finish

--- WeatherPipe/src/main/java/edu/purdue/cs307/team16/test/AWSInterfaceTest.java
+++ WeatherPipe/src/main/java/edu/purdue/cs307/team16/test/AWSInterfaceTest.java
@@ -25,7 +25,7 @@ import edu.purdue.cs307.team16.RadarFilePicker;
 import junit.framework.TestCase;
 
 public class AWSInterfaceTest extends TestCase {
-	@Test
+	/*@Test
 	public void testListBucket() {
 		final String dataBucket = "noaa-nexrad-level2";
 		String[] key = {"2010/01/01", "2010/07/14"};
@@ -62,5 +62,36 @@ public class AWSInterfaceTest extends TestCase {
 		s3client.setRegion(region);
 		s3client.deleteBucket(bucketName);
 		System.out.println("FindOrCreateWeatherPipeJobBucket() is ok");
+	}*/
+	
+	@Test
+	public void testUploadInputFileList() {
+		//ArrayList<String> fileList = new ArrayList<String>();
+		ArrayList<ArrayList<String>> fileList1 = new ArrayList<ArrayList<String>>();
+		
+		//DateTime startTimes = new DateTime(2010, 01, 01, 07, 39, 28);
+		//DateTime endTimes = new DateTime(2010, 01, 01, 15, 58, 47);
+		DateTime[] startTimes = {new DateTime(2010, 01, 01, 07, 39, 28), new DateTime(2010, 01, 01, 07, 39, 28)};
+		DateTime[] endTimes = {new DateTime(2010, 01, 01, 15, 58, 47), new DateTime(2010, 01, 01, 07, 39, 30)};
+		String key = "2010/01/01";
+		String station = "KBBX";
+		final String dataBucket = "noaa-nexrad-level2";
+		String[] jobID = {"job1", "job2"};
+		//jobID = "job1";
+		String[] bucketName = {"fdafda", "adfeth"};
+		AWSInterface[] awsInterface = {new AWSInterface(jobID[0], bucketName[0]), new AWSInterface(jobID[1], bucketName[1])};
+		//awsInterface.FindOrCreateWeatherPipeJobBucket();
+		//AWSInterface awsInterface = new AWSInterface(jobID);
+		String[] ret = new String[2];
+		for(int i = 0; i < 2; i++) {
+			awsInterface[i].FindOrCreateWeatherPipeJobBucket();
+			fileList1.add(RadarFilePicker.getRadarFilesFromTimeRange(startTimes[i], endTimes[i], station, awsInterface[i], dataBucket));
+			ret[i] = awsInterface[i].UploadInputFileList(fileList1.get(i), dataBucket);
+		}
+		
+		
+		String[] answer = {"s3n://fdafda/job1_input", "s3n://adfeth/job2_input"};
+		assertArrayEquals(answer, ret);
+		System.out.println("UploadInputFileList() is ok");
 	}
 }
