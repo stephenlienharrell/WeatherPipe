c0113dbf638bf3da0d79cd6d4c7ba4c1893f81e7
Xiaoyang Lin <lin430@purdue.edu>
2015-12-03 17:34:56 -0500

    add config file and config file editor, please put the file WeatherPile.ini into the directory /build/distribution after building the whole program

---
+++ WeatherPipe/src/main/java/edu/purdue/cs307/team16/INIEditor.java
@@ -0,0 +1,39 @@
+package edu.purdue.cs307.team16;
+
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class INIEditor {
+	String path = null;
+	FileInputStream in = null;
+	FileOutputStream out;
+	Properties p;
+
+
+	public void read(String filename) throws IOException {
+		path = filename;
+		p = new Properties();
+		in = new FileInputStream(path);
+		p.load(in);
+	}
+
+	public void setValue(String key, String value) throws IOException{
+		out = new FileOutputStream(path);
+		p.setProperty(key, value);
+		p.store(out, null);
+		out.close();
+	}
+	
+	public boolean hasKey(String key){
+		return p.containsKey(key);
+	}
+	
+	public String getValue(String name) {
+		String value = p.getProperty(name);
+		return value;
+	}
+	
+}
